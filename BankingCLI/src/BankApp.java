38335c30cc73f00f765cfaa98aad6a33c6ce819d
bartoszzap <bzaporowicz1@gmail.com>
2025-10-14 20:41:15 +0100

    Update BankApp.java

--- BankingCLI/src/BankApp.java
+++ BankingCLI/src/BankApp.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class BankApp {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Bank bank = new Bank();
+
+    public static void main(String[] args) {
+
+    }
 }
